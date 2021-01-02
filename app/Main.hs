175a42fe9cc59abbe8bb4962772bdc07aa107bb9
George Thomas <georgefsthomas@gmail.com>
2021-01-02 01:05:10 +0000

    Initial implementation of "collapse-groups" plugin

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import Graphics.SvgTree (loadSvgFile, saveXmlFile)
+import Svgone.Processors.CollapseGroups qualified as CollapseGroups
 import Svgone.Processors.MergePaths qualified as MergePaths
 import Svgone.Types
 import System.Environment (getArgs)
@@ -10,6 +11,10 @@ main =
     getArgs >>= \case
         [inF, outF] ->
             loadSvgFile inF >>= \case
-                Just in' -> saveXmlFile outF $ plugin @MergePaths.P defaultOpts in'
+                Just in' ->
+                    saveXmlFile outF
+                        . plugin @MergePaths.P defaultOpts
+                        . plugin @CollapseGroups.P defaultOpts
+                        $ in'
                 Nothing -> error "couldn't read input"
         _ -> error "bad args"
--- src/Svgone/Processors/CollapseGroups.hs
+++ src/Svgone/Processors/CollapseGroups.hs
@@ -1,1 +1,29 @@
 module Svgone.Processors.CollapseGroups where
+
+import Control.Lens
+import Control.Monad
+import Data.Maybe
+import Graphics.SvgTree
+import Svgone.Types
+
+data P
+type Opts = PluginOptions P
+
+instance Plugin P where
+    data PluginOptions P = Opts
+    defaultOpts = Opts
+    plugin :: Opts -> Document -> Document
+    plugin Opts = documentElements %~ trees
+    pluginName = "collapse-groups"
+
+trees :: [Tree] -> [Tree]
+trees = map $ branch . (^. treeBranch)
+
+branch :: TreeBranch -> Tree
+branch t = case t of
+    GroupNode g -> fromMaybe (Tree $ GroupNode $ g & groupChildren %~ trees) do
+        [Tree (PathNode p)] <- pure $ g ^. groupChildren
+        guard $ isNothing $ g ^. groupViewBox
+        guard $ g ^. groupAspectRatio == defaultSvg
+        pure $ Tree $ PathNode p & drawAttributes %~ ((g ^. groupDrawAttributes) <>)
+    x -> Tree x
